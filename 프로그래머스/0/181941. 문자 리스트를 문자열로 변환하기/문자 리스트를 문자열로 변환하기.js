333c1db6ad3ed59b191879344b62b24e222fa833
Dang <56210432+looks32@users.noreply.github.com>
2024-11-24 02:31:04 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
@@ -1,8 +1,3 @@
 function solution(arr) {
-    var answer = '';
-    const leng = arr.length;
-    for(i=0; i<leng; i++){
-        answer += arr[i];
-    }
-    return answer;
+    return arr.join("");
 }
