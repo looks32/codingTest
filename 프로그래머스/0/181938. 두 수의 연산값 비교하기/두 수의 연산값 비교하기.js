1a1c0f8d751eb82c0bd74ffa0c9d7afe44e75d28
Dang <56210432+looks32@users.noreply.github.com>
2024-11-25 21:28:53 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,13 +1,3 @@
 function solution(a, b) {
-    var answer = 0;
-    
-    if( String(a) + String(b) < 2 * a * b){
-        answer =  2 * a * b
-    } else {
-        answer = Number(String(a) + String(b))
-    }
-    
-    return answer;
-    
-    
+    return Math.max(+(a.toString()+b.toString()), 2 * a * b)
 }
