3dc7d9474dee67ef993b2cb1b62b86d373442549
Dang <56210432+looks32@users.noreply.github.com>
2024-11-30 23:18:19 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -1,7 +1,3 @@
 function solution(my_string, n) {
-    var answer = '';
-    const max = my_string.length
-    const min = max - n 
-    answer = my_string.slice(min,max);
-    return answer;
+    return answer = my_string.substring(my_string.length - n);
 }
