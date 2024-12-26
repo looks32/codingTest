3e4857a0e269b42d42ea2c57617349f46c0aeec2
Dang <56210432+looks32@users.noreply.github.com>
2024-12-26 11:53:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    var answer = '';
-    var arr = my_string.split('')
-    for(let i = 0; i <arr.length; i++){
-        if(arr[i].match(new RegExp(/^[A-Z]/))){
-            answer += arr[i].toLowerCase();
-        }else{
-            answer += arr[i].toUpperCase();
-        }
-    }
-    return answer;
+    return my_string.split('').map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('')
 }
