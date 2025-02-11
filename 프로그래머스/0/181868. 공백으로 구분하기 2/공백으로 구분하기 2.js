65b6935efa686bf683c87c9ecda112d4205b1bcf
Dang <wheodnjs2@naver.com>
2025-02-11 15:45:47 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    var answer = [];
-    answer = my_string.trim().split(' ').filter(v => v != "")
-    return answer;
+    return my_string.split(' ').filter(v => v != "")
 }
