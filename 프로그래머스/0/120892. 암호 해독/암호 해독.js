380370c2dba3dd50bd9bc55f104ae5c68662bc18
Dang <wheodnjs2@naver.com>
2025-08-29 11:25:48 +0900

    [level 0] Title: 암호 해독, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,10 @@
+function solution(cipher, code) {
+    var answer = [];
+    // cipher = cipher.trim().split('');
+    for(let i = 1; i <= cipher.length; i++){
+        if(i % code === 0){
+            answer += cipher[i-1]
+        }
+    }
+    return answer;
+}
