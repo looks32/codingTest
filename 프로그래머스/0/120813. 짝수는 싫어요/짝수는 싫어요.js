9e1f0cbc39634b68c662d684c8bb103c91d538a2
Dang <wheodnjs2@naver.com>
2025-10-23 22:37:57 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,7 @@
 function solution(n) {
     var answer = [];
-    for(let i = 0; i<=n; i++){
-        if(i%2 !== 0){
-            answer.push(i)
-        }
+    for(let i = 1; i<=n; i+=2){
+        answer.push(i);
     }
     return answer;
 }
