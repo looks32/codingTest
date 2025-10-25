05365d087caa0622a5dc98ec8da184aed8d3af08
Dang <wheodnjs2@naver.com>
2025-10-25 22:53:54 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,7 +1,10 @@
 function solution(n) {
+    let pizza = 1;
+    
     for(let i = 1; i <= n; i++){
-        if(Number.isInteger(i * 6 / n)){
-            return i
+        if(6 * pizza % n === 0){
+            return pizza
         }
+        pizza++;
     }
 }
