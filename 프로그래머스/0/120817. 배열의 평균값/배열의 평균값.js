c71ef2f3f21168ef0927afc2668f137aba3300a0
Dang <wheodnjs2@naver.com>
2025-10-27 21:50:27 +0900

    [level 0] Title: 배열의 평균값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
     var answer = 0;
-    answer = numbers.reduce((a, b) => a + b)
-    return answer / numbers.length;
+    answer = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+    return answer 
 }
