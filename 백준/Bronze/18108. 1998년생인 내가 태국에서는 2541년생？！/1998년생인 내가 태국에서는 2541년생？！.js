8d30f728b316a11f4e5c97b402754e57e2b8d642
Dang <wheodnjs2@naver.com>
2025-12-10 16:39:31 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
-const [A] = fs.readFileSync(0, 'utf-8').trim().split(' ').map(Number);
+const path = require('path');
 
-console.log(A - 543);
+const src =
+  process.platform === 'linux'
+    ? 0
+    : process.env.LOCAL_INPUT || path.join(__dirname, 'input.txt');
+const raw = fs.readFileSync(src, 'utf8').replace(/\r/g, '').trim();
+
+const lines = raw.split('\n'); // 줄 단위가 필요할 때
+const tokens = raw.split(/\s+/); // 공백 단위가 필요할 때
+const nums = tokens.map(String); // 전부 숫자면 이 한 줄로도 OK
+
+// --- 여기부터 풀이 ---
+
+console.log(nums[0] - 543);
