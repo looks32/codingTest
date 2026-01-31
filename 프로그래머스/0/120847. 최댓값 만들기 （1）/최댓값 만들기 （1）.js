91ad235dd8010283a4a523de0997994fa745329d
Dang <wheodnjs2@naver.com>
2026-01-31 17:13:34 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,7 +1,5 @@
 function solution(numbers) {
-    let answer = 0;
-    let arr = numbers.sort((a, b) => a - b);
-    let lang = arr.length
-    answer = Number(arr[lang-1] * arr[lang-2])
-    return answer
+    numbers.sort((a,b) => a - b);
+    const answer =  numbers[numbers.length - 1] * numbers[numbers.length - 2]
+    return answer;
 }
