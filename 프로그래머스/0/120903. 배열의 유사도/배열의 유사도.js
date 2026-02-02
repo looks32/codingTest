a6fb5093d2e35fb150287cb4a69dbc79436629b1
Dang <wheodnjs2@naver.com>
2026-02-02 15:29:48 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,3 @@
 function solution(s1, s2) {
-    var answer = 0;
-    for(let i = 0; i<s1.length; i++){
-        for(let j = 0; j<s2.length; j++){
-            if(s1[i] === s2[j]){
-                answer++
-            }
-        }
-    }
-    return answer;
+    return s1.filter((v) => s2.includes(v)).length;
 }
