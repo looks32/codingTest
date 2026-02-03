1372ff6b40129494f6b0592ddecec9487f76d331
Dang <wheodnjs2@naver.com>
2026-02-03 18:43:18 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,5 +1,5 @@
 function solution(n, numlist) {
     var answer = [];
-    numlist.forEach(arr => (arr % n === 0) && answer.push(arr));
+    answer = numlist.filter((v) => v % n === 0);
     return answer;
 }
