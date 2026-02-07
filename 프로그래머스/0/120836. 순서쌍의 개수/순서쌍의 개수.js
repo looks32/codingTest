87dad7521812028e8ec4f0903278b710aab50dc7
Dang <wheodnjs2@naver.com>
2026-02-07 10:33:13 +0900

    [level 0] Title: 순서쌍의 개수, Time: 30.96 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    var answer = [];
-    for(let i = 1; i <= n; i++) {
-        if(n % i === 0) {
-            answer.push(i);
-        }
-    }
-    return answer.length;
+  let answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+      if (n % i === 0) {
+        answer += 1;
+      }
+  }
+  return answer;
 }
