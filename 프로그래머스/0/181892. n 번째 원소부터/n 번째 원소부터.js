ac1e314fba64511d2140ad48962c4d8438b344c2
Dang <wheodnjs2@naver.com>
2026-02-09 20:24:39 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,7 +1,3 @@
 function solution(num_list, n) {
-    var answer = [];
-    for(let i = n-1; i < num_list.length; i++){
-        answer.push(num_list[i]);
-    }
-    return answer;
+    return num_list.filter((_, i) => i >= (n-1));
 }
