20ca1e975d2ab36f439de160eef3b4e274bb7e6b
Dang <wheodnjs2@naver.com>
2026-02-12 23:54:19 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,14 +1,5 @@
 function solution(num_list) {
-    let answer = 0;
-    let a = [];
-    let b = [];
-    for(let i = 0; i<num_list.length; i++){
-        if(num_list[i] % 2 === 0){
-            a.push(num_list[i])
-        } else {
-            b.push(num_list[i])
-        }
-        answer = Number(a.join('')) + Number(b.join(''))
-    }
-    return answer;
+    const even = num_list.filter((v) => v % 2 !== 0).join('');
+    const odd = num_list.filter((v) => v % 2 === 0).join('');
+    return +even + +odd;
 }
