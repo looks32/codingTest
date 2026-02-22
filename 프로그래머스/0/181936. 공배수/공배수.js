1c0f1dbf47b644126d95f6f754b97a838dd7afd8
Dang <wheodnjs2@naver.com>
2026-02-22 23:13:13 +0900

    [level 0] Title: 공배수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.js
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -1,6 +1,3 @@
 function solution(number, n, m) {
-    var answer = 0;
-    number % n === 0 && number % m === 0 ? answer = 1 : answer = 0
-    return answer;
-
+    return number % n === 0 && number % m === 0 ? +true : +false;
 }
