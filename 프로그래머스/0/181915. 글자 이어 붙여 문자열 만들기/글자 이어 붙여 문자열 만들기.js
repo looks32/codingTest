684ef6b1ad8334fa0d65e92fa7a821361ac0cfdf
Dang <wheodnjs2@naver.com>
2026-03-05 12:38:06 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,4 +1,3 @@
 function solution(my_string, index_list) {
-    return index_list.map(i => my_string[i]).join('')
-    return index_list.map(i => my_string[i]).join()
+    return index_list.map((v)=> my_string[v]).join('');
 }
