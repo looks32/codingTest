9a087e409f677d98ce6ac344f88e64271369e661
Dang <wheodnjs2@naver.com>
2026-03-08 22:12:12 +0900

    [level 0] Title: A 강조하기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,5 +1,3 @@
 function solution(myString) {
-    var answer = '';
-    myString.split('').map((e) => e === 'a' || e === 'A' ? answer += e.toUpperCase() : answer += e.toLowerCase());
-    return answer;
+    return myString.split('').map((v) => v === 'a' || v === 'A' ? v.toUpperCase() : v.toLowerCase()).join('');
 }
